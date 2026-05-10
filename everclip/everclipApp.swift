80eb5fe4d8d65f730111b294c4a2597edc7595e7
SampleUser <sample@example.com>
2026-05-10 10:56:21 +0800

    Initial Commit

---
+++ everclip/everclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  everclipApp.swift
+//  everclip
+//
+//  Created by sudoevolve on 2026/5/10.
+//
+
+import SwiftUI
+
+@main
+struct everclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
